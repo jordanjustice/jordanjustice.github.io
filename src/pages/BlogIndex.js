872edc352840e41159cae748f103abdcac792e0e
Jordan Justice <jordi2108@gmail.com>
2018-03-22 09:41:24 -0400

    removes typography.js rhythm call

--- src/pages/BlogIndex.js
+++ src/pages/BlogIndex.js
@@ -15,11 +15,7 @@ class BlogIndex extends React.Component {
           const title = get(node, 'frontmatter.title') || node.fields.slug
           return (
             <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
+              <h3>
                 <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
                   {title}
                 </Link>
